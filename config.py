3298a9d4728f59c00c6e470889e3a6fc9aa179be
allentertainmentsstudio <allentertainmentsstudio@gmail.com>
2026-03-26 12:39:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,14 +12,14 @@ API_ID = int(getenv("API_ID", 34446649))
 API_HASH = getenv("API_HASH", "8dc570c08d8e35e88fb9bfc73c65d7fa")
 BOT_TOKEN = getenv("BOT_TOKEN", "8799486461:AAH6td7_S68a3PnGNJermUgxA6_A1vhBS-c")
 
-OWNER_ID = int(getenv("OWNER_ID", 7168219724)
+OWNER_ID = int(getenv("OWNER_ID", 7168219724))
 OWNER_USERNAME = getenv("OWNER_USERNAME", "Anujedits7")
 BOT_USERNAME = getenv("BOT_USERNAME", "Media_downloader_ak_bot")
 BOT_NAME = getenv("BOT_NAME", "Annie Music")
 ASSUSERNAME = getenv("ASSUSERNAME", "Anujedits76")
 
 # ── Database & logging ─────────────────────────────────────────────────────────
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://Anujedit:Anujedit@cluster0.7cs2nhd.mongodb.net/?appName=Cluster0"
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://Anujedit:Anujedit@cluster0.7cs2nhd.mongodb.net/?appName=Cluster0")
 LOGGER_ID = int(getenv("LOGGER_ID", -1003791508617))
 
 # ── Limits (durations in min/sec; sizes in bytes) ──────────────────────────────
